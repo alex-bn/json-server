520ede48ee949712faf0fc4557ffc6addb967b00
Alex Bajan <alex.i.bajan@gmail.com>
2021-12-29 23:44:26 +0200

    json server

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// js for index.html
+
+const container = document.querySelector('.blogs');
+const searchForm = document.querySelector('.search');
+
+const renderedPosts = async term => {
+  let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+
+  if (term) {
+    uri += `&q=${term}`;
+  }
+
+  const res = await fetch(uri);
+  const posts = await res.json();
+  console.log(posts);
+
+  let template = '';
+
+  // inject posts
+  posts.forEach(post => {
+    template += `
+      <div class="post">
+      <h2>${post.title}</h2>
+      <p><small>${post.likes} likes</small></p>
+      <p>${post.body.slice(0, 200)}</p>
+      <a href="/details.html?id=${post.id}">read mode...</a>
+      </div>
+    `;
+  });
+
+  container.innerHTML = template;
+};
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  renderedPosts(searchForm.term.value.trim());
+});
+
+window.addEventListener('DOMContentLoaded', () => renderedPosts());
